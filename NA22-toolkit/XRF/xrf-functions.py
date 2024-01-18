595dcc46213d6a97e3a6e0136ce14eefc89f6ebc
Dominic Piedmont <144847957+drpiedmont@users.noreply.github.com>
2024-01-18 12:38:22 -0500

    Update xrf-functions.py

--- NA22-toolkit/XRF/xrf-functions.py
+++ NA22-toolkit/XRF/xrf-functions.py
@@ -740,11 +740,7 @@ def AOI_extractor(filename, min_energy, elements, AOI_x, AOI_y, BKG_x, BKG_y, pr
     peaks, properties = find_peaks(AOI_bkg_sub, prominence = prom, height = height, distance = dist)
         
 
-    ########## Identify elements ##########
-    # identify fluorescent line energy that most closely matches the determined peaks
-    tolerance = 1.5 # allowed difference in percent
-    matched_peaks = identify_element_match(elements, energy_int[peaks]*1000, tolerance)
-    
+       
     
     ########## Fit spectra and plot results ##########
     print('Beginning peak fitting')
@@ -756,6 +752,11 @@ def AOI_extractor(filename, min_energy, elements, AOI_x, AOI_y, BKG_x, BKG_y, pr
     # Label peaks
     labels = []
     for i in range(len(peaks)): labels.extend(['Peak '+str(i+1)])
+
+    ########## Identify elements ##########
+    # identify fluorescent line energy that most closely matches the determined peaks
+    tolerance = 1.5 # allowed difference in percent
+    matched_peaks = identify_element_match(elements, energy_int[peaks]*1000, tolerance)
 
     ########## Final Plot ##########
     fig1 = go.Figure(data = go.Scatter(x = energy_int, y = AOI, mode = 'lines', name = 'AOI Spectra'), layout_xaxis_range = [min_energy,max_energy])
